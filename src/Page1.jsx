b1d14b6d699b198ad15cc621975b9da5165741d2
USERNAME <shinozakiyume88@gmail.com>
2023-03-20 13:21:18 +0900

    ネストされたページ遷移

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,7 +1,12 @@
-export const  Page1 = () => {
+import { Link } from "react-router-dom";
+
+export const Page1 = () => {
   return (
     <div>
       <h1>Page1です</h1>
+      <Link to="/page1/detailA">Page1DetailA</Link>
+      <br />
+      <Link to="/page1/detailB">Page1DetailB</Link>
     </div>
   );
 };
