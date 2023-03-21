bec30c39078c7f052fe88570e533866d898b26fb
USERNAME <shinozakiyume88@gmail.com>
2023-03-21 16:42:24 +0900

    クエリパラメータを扱う

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,16 +1,12 @@
-export const  Page2 = () => {
-  return (
-    <div>
-      <h1>Page2です</h1>
-    </div>
-  );
-};import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Page2 = () => {
   return (
     <div>
       <h1>Page2です</h1>
       <Link to="/page2/100">URL Parameter</Link>
+      <br />
+      <Link to="/page2/100?name=hogehoge">Query Parameter</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,11 +1,14 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
   return (
     <div>
       <h1>UrlParameterです</h1>
-      <p>パラメーターは{ id }です</p>
+      <p>パラメーターは　{ id }です</p>
+      <p>クエリパラメーターは　{ query.get("name") }です</p>
     </div>
   );
 };
